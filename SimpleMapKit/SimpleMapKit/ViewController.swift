d393e102b692151a2d8baee2f253ce14fad697fc
Jayve Javier <83806412+jayvejavier@users.noreply.github.com>
2021-05-07 14:58:46 +0800

    Add files via upload

---
+++ SimpleMapKit/SimpleMapKit/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  SimpleMapKit
+//
+//  Created by Mospeng Research Lab Philippines on 2/19/20.
+//  Copyright © 2020 Mospeng Research Lab Philippines. All rights reserved.
+//
+
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+        let coordinate = CLLocationCoordinate2DMake(37.331652997806785, -122.03072304117417)
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.003, longitudeDelta: 0.003)
+        let region = MKCoordinateRegion.init(center: coordinate, span: span)
+        mapView.setRegion(region, animated:true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "title"
+        annotation.subtitle = "subtitle"
+        self.mapView.addAnnotation(annotation)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - MKMapView delegate
+
+    // Called when the region displayed by the map view is about to change
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        print(#function)
+    }
+
+    // Called when the annotation was added
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.animatesDrop = true
+            pinView?.canShowCallout = true
+            pinView?.isDraggable = true
+            pinView?.pinColor = .purple
+
+            let rightButton: AnyObject! = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            pinView?.rightCalloutAccessoryView = rightButton as? UIView
+        }
+        else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        print(#function)
+        if control == view.rightCalloutAccessoryView {
+            performSegue(withIdentifier: "toTheMoon", sender: self)
+        }
+    }
+
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationView.DragState, fromOldState oldState: MKAnnotationView.DragState) {
+        if newState == MKAnnotationView.DragState.ending {
+            let droppedAt = view.annotation?.coordinate
+            print(droppedAt.debugDescription)
+        }
+    }
+
+    // MARK: - Navigation
+
+    @IBAction func didReturnToMapViewController(_ segue: UIStoryboardSegue) {
+        print(#function)
+    }
+}
+
+
+
